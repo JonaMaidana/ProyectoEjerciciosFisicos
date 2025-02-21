02a5edca26ac955b989a8f87b7f8b4d3ceb41466
JonaMaidana <jonamaidana18@gmail.com>
2025-02-21 00:51:36 -0300

    Proyecto MVC

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ProyectoEjerciciosFisicos.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
